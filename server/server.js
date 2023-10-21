402f353a9f520ad342d43f7a16c8f2643cc54f25
DILIP SANAP <dilipinbsanap@gmail.com>
2023-10-21 21:12:40 +0530

    database connected successfully

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT;
+const connection=require('./database/db')
 const app = express();
+
 const server=require('http').createServer(app)
 const io = require('socket.io')(server);
 
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+    try {
+        await connection;
+        console.log('Successfully connected to DB');
+    } catch (error) {
+        console.log('Error in connecting the DB: ',error);
+    }
     console.log(`Server is running on port ${PORT}`);
 })
