d7363b2a0f666c51b5123f3ec9ef3f9ad7d58326
DILIP SANAP <dilipinbsanap@gmail.com>
2023-10-23 11:59:55 +0530

    Navbar component created

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './components/authentication/SignUp';
 import TasksPage from './pages/TasksPage';
+import AssignTask from './components/tasks/AssignTask';
+import CompletedTask from './components/tasks/CompletedTask';
+import GetProfile from './components/authentication/GetProfile';
 
 function App() {
   return (
@@ -12,8 +15,10 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/tasks" element={<TasksPage />} />
+      <Route path="/assign-tasks" element={<AssignTask />} />
+      <Route path="/completed-tasks" element={<CompletedTask />} />
+      <Route path="/profile" element={<GetProfile />} />
     </Routes>
-
   );
 }
 
---
+++ client/src/components/authentication/GetProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+const GetProfile = () => {
+    return (
+        <>
+            <Navbar />
+            Get A Profile
+        </>
+    )
+};
+
+export default GetProfile
