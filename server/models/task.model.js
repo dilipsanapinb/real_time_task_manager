eb68a19a79f9595cc6961c4216df8ed89cc58221
DILIP SANAP <dilipinbsanap@gmail.com>
2023-10-24 16:16:25 +0530

    tasks model updated

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -1,28 +1,36 @@
 const mongoose = require('mongoose');
 
-const taskSchema = mongoose.Schema({
-    project: {
-        type: String,
-        required: true,
+const taskSchema = mongoose.Schema(
+    {
+        project: {
+            type: String,
+            required: true,
+        },
+        assignedTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        assignedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        completed: {
+            type: Boolean,
+            default: false,
+        },
     },
-    assignedTo: {
-        type: mongoose.Schema.Types.ObjectId,
-        'ref': 'User',
-        required:true
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    completed: {
-        type: Boolean,
-        default:false,
-    }
-}, {timestamps: true});
+    { timestamps: true }
+);
 
 const Task = mongoose.model('task', taskSchema);
 module.exports = Task;
